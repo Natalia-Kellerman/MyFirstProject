74b56166e31c79ef79d781430e72c3afb6ffd268
Natalia-Kellerman <89622471+Natalia-Kellerman@users.noreply.github.com>
2021-09-01 08:20:47 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+Natalia Kellerman 
+C++ Fall 2021
+Due: Wednesday, September 1st
+Lab 1 HelloWorld
+Diplay Hello World and favorite color to the screen
+*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "Blue";
+	return 0;
+}
